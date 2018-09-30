91c31fc1414fbb41576f754596a3eb4df2615283
Polszenager <janicasio80@gmail.com>
2018-09-30 12:46:48 +0200

    Added universe class

---
+++ Genetic/Universe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Genetic
+{
+    class Universe
+    {
+        //List of avaible worlds
+        public List<World> Worlds_In_Universe;
+
+        //Number of the session
+        public int Session_Number;
+
+        public Universe (int _Session, List<World> _Worlds)
+        {
+            Worlds_In_Universe = _Worlds;
+            Session_Number = _Session;
+        }
+
+    }
+}
